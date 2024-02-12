4959964d0569bd1b19377228a2bb22cb4351c7f0
avigail1 <149592270+avigail1@users.noreply.github.com>
2024-02-12 16:34:42 +0200

    second commit

--- src/components/meeting/Meeting.jsx
+++ src/components/meeting/Meeting.jsx
@@ -2,22 +2,34 @@ import { observer } from "mobx-react";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import PersonIcon from '@mui/icons-material/Person';
 import EventRoundedIcon from '@mui/icons-material/EventRounded';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import dataStore from "../../data/dataStore";
 
 const Meeting = observer((value) => {
   const date = (e) => {
     return `${e.d}/${e.m}/${e.y} at ${e.h}:${e.minutes} `;
   }
+
+
+
   return (
     <>
-          <EventRoundedIcon/>
-          <span>{dataStore.serviceNumber}</span>
-          <PersonIcon/>
-          <h4>{value.clientName}</h4>
-          <h6>{value.clientEmail}</h6>
-          <AccessTimeIcon/>
-          <p>{date(value.dateTime)}</p>
-
+      <div className="meeting-row">
+        <EventRoundedIcon className="meeting-icon" />
+        <span>{value.serviceNumber}</span>
+      </div>
+      <div className="meeting-row">
+        <PersonIcon className="meeting-icon" />
+        <span>{value.clientName}</span>
+      </div>
+      <div className="meeting-row">
+        <AlternateEmailIcon/>
+        <span>{value.clientEmail}</span>
+      </div>
+      <div className="meeting-row">
+        <AccessTimeIcon className="meeting-icon" />
+        <span>{date(value.dateTime)}</span>
+      </div>
     </>
   )
 })
